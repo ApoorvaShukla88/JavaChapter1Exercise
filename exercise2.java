8d7b2fa6dd693a43741f8bf8c772dd18630aad22
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-05 15:13:06 -0400

    exercise files

---
+++ exercise2.java
@@ -0,0 +1,16 @@
+package chapter1;
+ 
+public class exercise2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Programming is fun");
+		System.out.println("Programming is fun");
+		System.out.println("Programming is fun");
+		System.out.println("Programming is fun");
+		System.out.println("Programming is fun");
+		
+	}
+	
+	
+}
